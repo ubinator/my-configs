81f7daed2ea75523920c5f70f3629cb6a8b20e8c
ubinator <ntadic001@gmail.com>
2025-02-18 16:07:26 +0100

    Improved .tmux.conf and bashrc-additions.sh

--- bashrc-additions.sh
+++ bashrc-additions.sh
@@ -4,7 +4,7 @@
 # You can source it as many times as you want.
 
 # Sets the CURRENT_HOME shell variable, which is used to keep track of the currently saved home.
-CURRENT_HOME=$HOME
+CURRENT_HOME="$HOME"
 # Defines an alias that sets the home directory to the current directory.
 alias sethome='CURRENT_HOME=$PWD ; echo -e "\033[0;32mHome saved."'
 # Defines an alias that moves the working directory to the home directory.
@@ -16,4 +16,14 @@ alias gitlog='git --no-pager log --oneline --reverse --decorate'
 alias gitloggraph='git log --oneline --decorate --graph --all'
 
 # This makes it so commands from different sessions all get put into the bash history file. Previously, only the most recently closed session got it's commands added.
-PROMPT_COMMAND='history -a'     # After every command, before displaying prompt (the green and blue thing with user + hostname), append unwritten history to history file. Shouldn't be a heavy operation, just appending single lines.
+commands_to_add_to_prompt_command="history -a"         # After every command, before displaying prompt, append unwritten history to history file.
+
+# Primitive check to determine if PROMPT_COMMAND is empty. If it isn't, add command with ;.
+# If it is, just overwrite with command.
+filtered_prompt_command=$(echo "$PROMPT_COMMAND" | sed -e "s/ //g")
+if [ "$filtered_prompt_command" == "" ]
+then
+       PROMPT_COMMAND="$commands_to_add_to_prompt_command"
+else
+       PROMPT_COMMAND="$PROMPT_COMMAND ; $commands_to_add_to_prompt_command"
+fi
